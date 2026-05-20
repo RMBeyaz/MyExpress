1e53e7d0b63ddfc14434aa94cdac1b23120a8ef1
Mert BEYAZ <r.mertbeyaz@hotmail.com>
2026-05-21 01:34:09 +0300

    Refine mobile menu panel styling

--- panel/fiyatlandirma.php
+++ panel/fiyatlandirma.php
@@ -71,7 +71,7 @@ foreach ($rows as $row) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fiyatlandırma | MyExpress Panel</title>
-    <link rel="stylesheet" href="../styles.css?v=20260521-request-panel-polish">
+    <link rel="stylesheet" href="../styles.css?v=20260521-mobile-menu-panel">
   </head>
   <body class="panel-body">
     <main class="panel-shell">
